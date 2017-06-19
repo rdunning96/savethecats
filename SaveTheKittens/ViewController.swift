6d51ee71ca9c204a169c0433c6376eb91580214e
Ryan Dunning <ryandunning@Ryans-MacBook-Air.local>
2017-06-19 14:59:16 -0400

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Ryan Dunning on 6/19/17.
+//  Copyright © 2017 Ryan Dunning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
